6f807fb09f1e7bb97e638fd09b4b57dfbf08ceef
gowdavsg <50978705+gowdavsg@users.noreply.github.com>
2019-05-24 15:51:09 +0530

    Add files via upload

--- shashankv2.c
+++ shashankv2.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
-int inputarray(int a[],int *n);
+int inputarray(int);
+int inputarrayy(int a[],int);
 int sumarray(int a[],int);
 int outputarray(int);
 int main()
 {
 	int n,s;
+	n=inputarray(n);
 	int a[n];
-	inputarray(a,&n);
+	inputarrayy(a,n);
 	s=sumarray(a,n);
 	outputarray(s);
 }
-int inputarray(int a[],int *n)
+int inputarray(int n)
+{
+	printf("\n Enter number of elements:");
+	scanf("%d",&n);
+	return n;
+}
+int inputarrayy(int a[],int n)
 {
 	int i;
-	printf("\n Enter number of elements:");
-	scanf("%d",n);
-	printf("\n Enter elements:");
-	for(i=0;i<*n;i++)
+	printf("\n Enter %d elements:",n);
+	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
